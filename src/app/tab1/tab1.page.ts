740dc1f43b147563220e20ecebafc944faef3676
DarkWister <101299728+darkwister@users.noreply.github.com>
2025-04-02 21:07:51 +0000

    app complete

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,13 +1,37 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
-import { ExploreContainerComponent } from '../explore-container/explore-container.component';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonItem, IonList, IonInput, IonButton, IonIcon } from '@ionic/angular/standalone';
+import { Router } from '@angular/router';
+import { addIcons } from 'ionicons';
+import { add } from 'ionicons/icons';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent],
+  imports: [IonIcon, IonButton, IonInput, IonList, IonItem, IonHeader, IonToolbar, IonTitle, IonContent, FormsModule],
 })
 export class Tab1Page {
-  constructor() {}
+  nombre: string = '';
+  apellido: string = '';
+  latitud: number | null = null;
+  longitud: number | null = null;
+  constructor(private router: Router) {
+    addIcons({ add })
+  }
+
+  enviarData(){
+    if (this.nombre && this.apellido && this.latitud && this.longitud !== null) {
+      this.router.navigate(['/tabs/tab2'], {
+        queryParams: {
+          nombre: this.nombre,
+          apellido: this.apellido,
+          latitud: this.latitud,
+          longitud: this.longitud,
+        },
+      });
+    } else {
+      alert('Por favor, completa todos los campos.');
+    }
+  }
 }
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,15 +1,92 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, OnDestroy } from '@angular/core';
 import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
-import { ExploreContainerComponent } from '../explore-container/explore-container.component';
+import * as L from 'leaflet';
+import { ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent]
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent ]
 })
-export class Tab2Page {
+export class Tab2Page implements AfterViewInit, OnDestroy{
+  nombre: string = '';
+  apellido: string = '';
+  latitud: number = 0;
+  longitud: number = 0;
+  direccion: string = 'Cargando...';
+  private map: L.Map | undefined;
+  private marker: L.Marker | undefined;
+  private routeSub: Subscription | undefined;
 
-  constructor() {}
+  constructor(private route: ActivatedRoute) {}
 
+  ngAfterViewInit(): void {
+    this.routeSub = this.route.queryParams.subscribe(params => {
+      this.nombre = params['nombre'];
+      this.apellido = params['apellido'];
+      this.latitud = parseFloat(params['latitud']) || 0;
+      this.longitud = parseFloat(params['longitud']) || 0;
+
+      if (!this.map) {
+        this.inicializarMapa();
+      }
+      this.actualizarMarker();
+      this.obtenerDireccion();
+      setTimeout(() => {
+        this.map?.invalidateSize();
+      }, 0);
+    });
+  }
+  inicializarMapa() {
+    this.map = L.map('map').setView([this.latitud, this.longitud], 15);
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; OpenStreetMap contributors',
+    }).addTo(this.map);
+
+    this.marker = L.marker([this.latitud, this.longitud], { icon: this.crearIcono() }).addTo(this.map);
+  }
+
+  actualizarMarker() {
+    if (this.marker) {
+      this.marker.setLatLng([this.latitud, this.longitud]);
+    } else {
+      this.marker = L.marker([this.latitud, this.longitud], { icon: this.crearIcono() }).addTo(this.map!);
+    }
+    this.map!.setView([this.latitud, this.longitud], 15);
+  }
+
+  crearIcono(){
+    return L.icon({
+      iconUrl: 'assets/leaflet/marker-icon.png',
+      shadowUrl: 'assets/leaflet/marker-shadow.png',
+      iconSize: [25, 41],
+      iconAnchor: [12, 41],
+      popupAnchor: [1, -34],
+      shadowSize: [41, 41]
+    });
+  }
+
+  obtenerDireccion() {
+    fetch(`https://nominatim.openstreetmap.org/reverse?format=json&lat=${this.latitud}&lon=${this.longitud}`)
+      .then(response => response.json())
+      .then(data => {
+        this.direccion = data.display_name || 'Ubicación desconocida';
+
+        if (this.marker) {
+          this.marker.unbindPopup();
+          this.marker.bindPopup(`<b>${this.nombre} ${this.apellido}</b><br>${this.direccion}`).openPopup();
+        }
+      })
+      .catch(() => {
+        this.direccion = 'No se pudo obtener la ubicación';
+    });
+  }
+  ngOnDestroy(): void {
+    if (this.routeSub) {
+      this.routeSub.unsubscribe();
+    }
+  }
 }
